789651bea2c3a1bc69098123ff9fe5701ea5546c
Susie Carovillano <scarovil@wellesley.edu>
2015-02-03 20:27:20 -0500

    initialize files

---
+++ am1.js
@@ -0,0 +1,8 @@
+
+var button = document.querySelector("button");
+
+button.onclick = function () {
+    var input = document.querySelector("#search");
+    console.log("Your search phrase is: ", input.value);
+}
+
