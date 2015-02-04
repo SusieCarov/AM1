21894f5209cc8eea7eeeb7753bbc4eff56bca263
Susie Carovillano <scarovil@wellesley.edu>
2015-02-03 20:30:36 -0500

    Update js file

--- am1.js
+++ am1.js
@@ -1,8 +1,34 @@
 
 var button = document.querySelector("button");
-
 button.onclick = function () {
-    var input = document.querySelector("#search");
-    console.log("Your search phrase is: ", input.value);
+  var input = document.querySelector("#search");
+  console.log("Your search phrase is: ", input.value);
 }
+
+var baseURL = "https://www.googleapis.com/books/v1/volumes?q=";
+var searchTerm = encodeURI("harry potter"); // this later needs to get the value from input
+var url = baseURL + searchTerm;
+
+function ajaxRequest(url) {
+  var request = new XMLHttpRequest(); 
+  request.open("GET", url); 
+  
+  request.onload = function() {
+    if (request.status == 200) {
+      displayBooks(request.responseText); // displayBooks is your function
+    }
+  };
+  
+  request.send(null); 
+}
+
+// Temporary function, to see that the request works
+function displayBooks(result){
+  alert("got results: " + result.length);
+  var results = JSON.parse(result);
+  console.log(results);
+}
+
+// invoke the function
+ajaxRequest(url);
 
