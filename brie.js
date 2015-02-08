80877f23dfba8495597a5605f50a0a84c1c6a9a8
Mary Ruth Ngo <mngo@wellesley.edu>
2015-02-08 16:53:04 -0500

    new button category capabilities

---
+++ brie.js
@@ -0,0 +1,127 @@
+var input;
+var thingy;
+var venueHolder;
+var apiKey = "4563ba26ae40bfc14b6f866baaaa038e6c927df7";
+var baseURL = "http://api.locu.com/v1_0/venue/search/?api_key=" 
+            + apiKey; //category actually won't be always restaurant!
+var urlWithLocation;
+
+var buttons = document.querySelectorAll("button");
+var locationButton = buttons[0];
+
+
+locationButton.onclick = function () {
+  var input = document.querySelector("#search");
+  input = input.value;
+  console.log(input);
+	if (input) {
+    var location = input.split(", ")
+    var city = encodeURI(location[0])
+    var state = encodeURI(location[1])
+    var url = baseURL + "&locality=" + city
+    url += "&region=" + state
+    url += "&callback=displayVenue"
+    jsonpRequest(url);
+
+	}
+  console.log("hello");
+  urlWithLocation = url;
+}
+
+//recreationButton, beautyButton, laundryButton, Fashion, restaurant, bookstore
+buttons[1].onclick = function () {
+  categoryRequest("recreation");
+}
+
+buttons[2].onclick = function () {
+  categoryRequest("beauty");
+}
+
+buttons[3].onclick = function () {
+  categoryRequest("laundry");
+}
+
+buttons[4].onclick = function () {
+  categoryRequest("fashion");
+}
+
+buttons[5].onclick = function () {
+  categoryRequest("bookstore");
+}
+
+function categoryRequest(category) {
+  var url = urlWithLocation.split("&c");
+  url[0] += "&category=" + category;
+  url[0] += "*callback=displayVenue";
+  jsonpRequest(url[0]);
+} 
+
+function jsonpRequest(requestURL) {
+  var newScriptElement = document.createElement("script");
+  newScriptElement.setAttribute("src", requestURL);
+  newScriptElement.setAttribute("id", "jsonp");
+  var oldScriptElement = document.getElementById("jsonp");
+  var head = document.getElementsByTagName("head")[0];
+  if (oldScriptElement == null) {
+    head.appendChild(newScriptElement);
+  }
+  else {
+    head.replaceChild(newScriptElement, oldScriptElement);
+  }
+
+function notAjaxRequest(url) {
+    
+    var newScriptElement = document.createElement("script");
+	newScriptElement.setAttribute("src", requestURL);
+	newScriptElement.setAttribute("id", "jsonp");
+	var oldScriptElement = document.getElementById("jsonp");
+	var head = document.getElementsByTagName("head")[0];
+	if (oldScriptElement == null) {
+		head.appendChild(newScriptElement);
+	}
+	else {
+		head.replaceChild(newScriptElement, oldScriptElement);
+	}
+}
+
+// Temporary function, to see that the request works
+var ourResult;
+function displayVenue(results){
+  ourResult = results;
+
+  if(venueHolder) {
+    venueHolder.innerHTML = "";
+  }
+  venueHolder = document.getElementById("searchResults");
+  for (i in results.objects)
+  {
+    console.log("inside for loop");
+    var venue = results.objects[i];
+    var div = document.createElement("div");
+    div.className = "Venue Info";
+
+    var phoneNum = document.createElement("div");
+    phoneNum.innerHTML = venue.phone;
+    phoneNum.className = "phone number";
+    console.log(phoneNum.innerHTML);
+
+    var name = document.createElement("h2");
+    name.innerHTML = venue.name;
+    console.log(name.innerHTML);
+
+    var address = document.createElement("h3");
+    address.innerHTML = venue.street_address;
+    console.log(address.innerHTML);
+
+    var website = document.createElement("h3");
+    website.innerHTML = venue.website_url;
+    console.log(website.innerHTML);
+
+    venueHolder.appendChild(div);
+    div.appendChild(phoneNum);
+    div.appendChild(name);
+    div.appendChild(address);
+    div.appendChild(website);
+  }
+
+}
