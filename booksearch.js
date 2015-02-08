e05b8c052a639e15582fd36d68f98fda00df1aab
Susie Carovillano <scarovil@wellesley.edu>
2015-02-08 17:16:48 -0500

    Finish Commenting book search stuff

--- booksearch.js
+++ booksearch.js
@@ -21,14 +21,15 @@ button.onclick = function () {
 	}
 }
 
+//Function that actually makes the request to google book api
 function ajaxRequest(url) {
   var request = new XMLHttpRequest(); 
   request.open("GET", url); 
   
   request.onload = function() {
-    if (request.status == 200) {
-      searchResults = (request.responseText);
-      displayBooks(searchResults); 
+    if (request.status == 200) { // if the request worked
+      var searchResults = (request.responseText); 
+      displayBooks(searchResults); //display the response in the html page
     }
   };
   
@@ -38,42 +39,55 @@ function ajaxRequest(url) {
 
 function displayBooks(result){
   var results = JSON.parse(result);
+  
+  // Gets the div that all results should be in
   bookHolder = document.getElementById("searchResults");
 
-  if(bookHolder) {
-    bookHolder.innerHTML = "";
+  if(bookHolder) { //used to make sure that bookHolder exists
+    bookHolder.innerHTML = ""; //clears any old input to make way for the new search results
   }
+    
   for (i in results.items)
   {
+      
+    //General book div and variable
     var book = results.items[i];
     var div = document.createElement("div");
     div.className = "bookInfo";
 
+    //Year of publication banner
     var pubDate = document.createElement("div");
     pubDate.innerHTML = book.volumeInfo.publishedDate.substring(0,4) || "Unknown";
     pubDate.className = "year";
 
+    // Book's Title
     var bookTitle = document.createElement("h2");
-    if (book.volumeInfo.title.length > 35)
+    if (book.volumeInfo.title.length > 35) //used to prevent title from exceeding two lines
         bookTitle.innerHTML = book.volumeInfo.title.substring(0,35).concat("...");
     else bookTitle.innerHTML = book.volumeInfo.title || "Unknown";
       
+    // Book's Author
     var author = document.createElement("h3");
     author.innerHTML = book.volumeInfo.authors[0] || "Unknown";
 
+    // Book's Cover Image
     var img = document.createElement("IMG");
     if (book.volumeInfo.imageLinks.thumbnail) {
       img.setAttribute("src", book.volumeInfo.imageLinks.thumbnail);
     } else {
-      img.setAttribute("src", "placeholder.jpg");
+      img.setAttribute("src", "placeholder.jpg"); //if there is no given picture
     }
 
+    // Book's Description
     var para = document.createElement("p");
-    if (book.volumeInfo.description.length > 200)
+    if (book.volumeInfo.description.length > 200) //used to prevent description from being too long
         para.innerHTML = book.volumeInfo.description.substring(0,200).concat("...");
     else para.innerHTML = book.volumeInfo.description;
 
+    // Place the bookInfo div in the search results section
     bookHolder.appendChild(div);
+    
+    // Place the individual bits of book information in the bookInfo div
     div.appendChild(pubDate);
     div.appendChild(bookTitle);
     div.appendChild(author);
